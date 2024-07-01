e7524d4cf1e64d2703eb6be0c87fd36d4a97a74a
Aditya Goswami <adityagoswami071@gmail.com>
2024-07-01 12:54:55 +0530

    feat: chart bar created

--- velocity-chart/src/components/BarChart.jsx
+++ velocity-chart/src/components/BarChart.jsx
@@ -1,18 +1,46 @@
 // create a rectangle box
 // have x-axis and y-axis
-// each data item maps to a bar 
+// each data item maps to a bar
 
-import React from 'react'
+import React, { useMemo } from "react";
 
-const BarChart = ({data}) => {
+const Bar = ({ name, height,ticket, colour }) => {
   return (
-        <div className="chart-container">
-            <div className="chart"></div>
-                <div className='y-axis-label'>Number of tickets</div>
-                <div  className='x-axis-label'>Department</div>
-            
-        </div>
-  )
-}
+    <div
+      className="bar"
+      style={{ backgroundColor: colour, height: `${height}%` }}
+    >
 
-export default BarChart
+      <div className="tooltip">
+        {name} -{ticket}
+      </div>
+    </div>
+  );
+};
+const BarChart = ({ data }) => {
+  const maxTicketCount = useMemo(() => {
+    return Math.max(...data.map((item) => item.ticket));
+  }, []);
+
+  return (
+    <div className="chart-container">
+      <div className="chart">
+        {data.map((item) => {
+          return (
+            <Bar
+            name={item.name}
+              height={(item.ticket / maxTicketCount) * 100}
+              colour={item.color}
+              ticket={item.ticket}
+            />
+          );
+        })}
+      </div>
+
+      <div className="y-axis-label">Number of tickets</div>
+      <div className="x-axis-label">Department</div>
+    </div>
+  );
+};
+
+export default BarChart;
